dd4973ddf5583ce60e63ef7b97d73cb4ac4a93c4
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2024-01-06 16:20:12 +0530

    CORS-related fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const { connect } = require("./config/database.config");
 const routes = require("./routes");
 
@@ -8,7 +8,13 @@ const server = express();
 const PORT = process.env.PORT || 8080;
 
 server.use(express.json());
-server.use(cors());
+server.use(
+  cors({
+    origin: ["https://oyo-clone-shirso.netlify.app", "http://127.0.0.1:3000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 server.use(cookieParser());
 
 server.get("/api", (req, res) => {
